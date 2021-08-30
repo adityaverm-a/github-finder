58903b014820ddcc46ae280653497e2aff731c6c
adityaverm-a <aditya.se.403@gmail.com>
2021-08-30 18:19:38 +0530

    pushing to github

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+    return (
+        <Fragment>
+            <h1>About Us</h1>
+            <p>This is an App to search Github Users</p>
+            <p>Version: 1.0.0</p>
+        </Fragment>
+    );
+};
+
+export default About;
